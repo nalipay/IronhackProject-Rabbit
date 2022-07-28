b0d5f61ce935fd4817f1e0f02cc5a65bee6d0588
Meryl Kirchhoff <meryl.kirchhoff@gmx.de>
2022-07-28 11:01:24 +0200

    changed file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ require("dotenv/config");
 // ℹ️ Connects to the database
 require("./db");
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -22,7 +25,14 @@ app.use("/api", allRoutes);
 const authRoutes = require("./routes/auth")
 app.use("/api/auth", authRoutes)
 
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html");
+  });
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 module.exports = app;
